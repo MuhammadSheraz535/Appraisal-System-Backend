719773e55d6532d359fdb5c503d4d480e268c17f
Muhammad Sheraz <mskhan7507@gmail.com>
2023-02-18 23:30:15 +0500

    in router package employee route  file is created

--- routes/router.go
+++ routes/router.go
@@ -1,11 +1,22 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mrehanabbasi/appraisal-system-backend/controller"
+)
 
 func NewRouter() *gin.Engine {
-	router := gin.Default()
+	r := gin.Default()
+	v1 := r.Group("/v1")
 
 	// TODO: Define/Call routes here
 
-	return router
+	uc := controller.New()
+	v1.POST("/employees", uc.CreateUser)
+	v1.GET("/employees", uc.GetUsers)
+	v1.GET("/employees/:id", uc.GetUser)
+	v1.PUT("/employees/:id", uc.UpdateUser)
+	v1.DELETE("/employees/:id", uc.DeleteUser)
+
+	return r
 }
