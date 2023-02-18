93926dd7efed2ebb3990888a8ffaa3ff6f40ce8c
Muhammad Sheraz <mskhan7507@gmail.com>
2023-02-18 23:31:44 +0500

    in controller package employee_controller  file is created

---
+++ controller/empl_controller.go
@@ -0,0 +1,98 @@
+package controller
+
+import (
+	"errors"
+
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/mrehanabbasi/appraisal-system-backend/database"
+	"github.com/mrehanabbasi/appraisal-system-backend/models"
+	"gorm.io/gorm"
+)
+
+type UserController struct {
+	Db *gorm.DB
+}
+
+func New() *UserController {
+	db := database.Connect()
+	db.AutoMigrate(&models.Employee{})
+	return &UserController{Db: db}
+}
+
+// create user
+func (uc *UserController) CreateUser(c *gin.Context) {
+	var user models.Employee
+	c.BindJSON(&user)
+	err := models.CreateUser(uc.Db, &user)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+// get users
+func (uc *UserController) GetUsers(c *gin.Context) {
+	var user []models.Employee
+	err := models.GetUsers(uc.Db, &user)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+// get user by id
+func (uc *UserController) GetUser(c *gin.Context) {
+	id, _ := strconv.Atoi(c.Param("id"))
+	var user models.Employee
+	err := models.GetUser(uc.Db, &user, id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{"error": "Record not found"})
+			return
+		}
+
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+// update user
+func (uc *UserController) UpdateUser(c *gin.Context) {
+	var user models.Employee
+	id, _ := strconv.Atoi(c.Param("id"))
+	err := models.GetUser(uc.Db, &user, id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+	c.BindJSON(&user)
+	err = models.UpdateUser(uc.Db, &user)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
+	c.JSON(http.StatusOK, user)
+}
+
+// delete user
+func (uc *UserController) DeleteUser(c *gin.Context) {
+	var user models.Employee
+	id, _ := strconv.Atoi(c.Param("id"))
+	_, err := models.DeleteUser(uc.Db, &user, id)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Record not found"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "User deleted successfully"})
+}
