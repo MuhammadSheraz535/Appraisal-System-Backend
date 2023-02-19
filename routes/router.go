f9a436311a979b1ed352167946701f4d28f83b47
MAsadjaved <masadjaved653@gmail.com>
2023-02-20 00:35:04 +0500

    Create Routers for methods

--- routes/router.go
+++ routes/router.go
@@ -1,11 +1,21 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/mrehanabbasi/appraisal-system-backend/controller"
+)
 
 func NewRouter() *gin.Engine {
 	router := gin.Default()
+	v1 := router.Group("/v1")
 
 	// TODO: Define/Call routes here
+	sc := controller.New()
+	v1.POST("/supervisors", sc.CreateUser)
+	v1.GET("/supervisors", sc.GetUsers)
+	v1.GET("/supervisors/:id", sc.GetUser)
+	v1.PUT("/supervisors/:id", sc.UpdateUser)
+	v1.DELETE("/supervisors/:id", sc.DeleteUser)
 
 	return router
 }
