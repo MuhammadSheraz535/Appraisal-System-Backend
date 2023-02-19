39357cc1cd65dfe19526676bc890abb3dc08e49a
Hamza Imtiaz <hamza.imtiaz47022@gmail.com>
2023-02-19 21:23:45 +0500

    changes in main and db file

--- main.go
+++ main.go
@@ -1,34 +1,3 @@
-// package main
-
-// import (
-// 	"log"
-// 	"os"
-
-// 	"github.com/joho/godotenv"
-// 	"github.com/mrehanabbasi/appraisal-system-backend/database"
-// 	"github.com/mrehanabbasi/appraisal-system-backend/routes"
-// )
-
-// func main() {
-// 	// Load environment variables from .env file
-// 	err := godotenv.Load(".env")
-// 	if err != nil {
-// 		log.Panicf("Error loading .env file: %v", err)
-// 	}
-
-// 	// Connect to the database
-// 	database.Connect()
-
-// 	// Register all the routes
-// 	server := routes.AttachRoutes()
-
-// 	port := os.Getenv("PORT")
-// 	if port == "" {
-// 		port = "8080"
-// 	}
-// 	server.Run(":" + port)
-// }
-
 package main
 
 import (
--- routes/router.go
+++ routes/router.go
@@ -1,36 +1,3 @@
-// package routes
-
-// import (
-// 	"github.com/gin-gonic/gin"
-// 	"gorm.io/gorm"
-
-// 	"github.com/mrehanabbasi/appraisal-system-backend/controller"
-// )
-
-// func AttachRoutes(r *gin.Engine, db *gorm.DB) {
-
-// 	router := gin.Default() 
-
-// 	roleController := controllers.NewRoleController(db)
-
-// 	v1 := router.Group("/v1")
-// 	{
-// 		roles := v1.Group("/roles")
-// 		{
-// 			roles.GET("", roleController.GetAllRoles)
-// 			roles.GET(":id", roleController.GetRoleByID)
-// 			roles.POST("", roleController.CreateRole)
-// 			roles.PUT(":id", roleController.UpdateRole)
-// 			roles.DELETE(":id", roleController.DeleteRole)
-// 		}
-// 	}
-
-// 	routes := router.Routes() // get the routes from the new router
-// 	for _, route := range routes {
-// 		r.Handle(route.Method, route.Path, route.HandlerFunc)
-// 	}
-// }
-
 package routes
 
 import (
