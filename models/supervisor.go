1253407ecfd88341d2c2ca280f230f1e3ee7e613
MAsadjaved <masadjaved653@gmail.com>
2023-02-19 19:35:57 +0500

    To make supervisor model

--- models/supervisor.go
+++ models/supervisor.go
@@ -1,1 +1,11 @@
 package models
+
+import "gorm.io/gorm"
+
+type Employee struct {
+	gorm.Model
+	E_ID  uint   `json:"employee_id" gorm:"PrimaryKey"`
+	Name  string `gorm:"size:60;not null" json:"name"`
+	Email string `gorm:"size:40;not null" json:"email"`
+	S_ID  uint   `json:"supervisor_id" gorm:"foreignKey:E_ID"`
+}
