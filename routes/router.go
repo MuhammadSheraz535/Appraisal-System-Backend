b47856b3607d483c21d7fb7ff818524bde21e72b
MAsadjaved <masadjaved653@gmail.com>
2023-02-22 21:32:39 +0500

    routes

--- routes/router.go
+++ routes/router.go
@@ -8,14 +8,15 @@ import (
 func NewRouter() *gin.Engine {
 	router := gin.Default()
 	v1 := router.Group("/v1")
-
 	// TODO: Define/Call routes here
-	sc := controller.New()
-	v1.POST("/supervisors", sc.CreateUser)
-	v1.GET("/supervisors", sc.GetUsers)
-	v1.GET("/supervisors/:id", sc.GetUser)
-	v1.PUT("/supervisors/:id", sc.UpdateUser)
-	v1.DELETE("/supervisors/:id", sc.DeleteUser)
+	sc := controller.NewSupervisorController()
+
+	v1.POST("/supervisors", sc.CreateSupervisor)
+	v1.GET("/supervisors", sc.GetSupervisors)
+	v1.GET("/supervisors/:id", sc.GetSupervisorByID)
+	v1.PUT("/supervisors/:id", sc.UpdateSupervisor)
+	v1.DELETE("/supervisors/:id", sc.DeleteEmployee)
+	v1.GET("/supervisor/:name", sc.GetSupervisorByName)
 
 	return router
 }
