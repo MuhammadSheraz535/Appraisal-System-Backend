f257a87d62f20cd861f754f93a4ad8be1ddd6ef8
MAsadjaved <masadjaved653@gmail.com>
2023-02-22 16:43:04 +0500

    Keep the database environment variable same

--- database/db.go
+++ database/db.go
@@ -4,25 +4,25 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
+var err error
 
-func Connect() {
-	godotenv.Load()
-	dbname := os.Getenv("MYSQL_DBNAME")
-	dbuser := os.Getenv("MYSQL_USER")
-	dbpassword := os.Getenv("MYSQL_PASSWORD")
-	dbhost := os.Getenv("MYSQL_HOST")
-	connection := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbuser, dbpassword, dbhost, dbname)
-	dbConnection, err := gorm.Open(mysql.Open(connection), &gorm.Config{})
+func Connect() *gorm.DB {
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbURL := os.Getenv("DB_URL")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
 
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUsername, dbPassword, dbURL, dbPort, dbName)
+
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("connection failed to the database ")
+		panic(err)
 	}
-	DB = dbConnection
-	fmt.Println("db conncted successfully")
+	return DB
 }
