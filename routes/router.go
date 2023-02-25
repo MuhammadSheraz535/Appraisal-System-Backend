964dd6d0d20d9c10472959085a22f22d4422df8f
MAsadjaved <masadjaved653@gmail.com>
2023-02-25 23:53:59 +0500

    define routes

--- routes/router.go
+++ routes/router.go
@@ -2,20 +2,20 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/mrehanabbasi/appraisal-system-backend/controller"
+	controllers "github.com/mrehanabbasi/appraisal-system-backend/controller"
 )
 
 func NewRouter() *gin.Engine {
 	router := gin.Default()
 	v1 := router.Group("/v1")
 	// TODO: Define/Call routes here
-	sc := controller.NewSupervisorController()
+	sc := controllers.NewSupervisorController()
 
-	v1.POST("/supervisors", sc.CreateSupervisor)
+	v1.POST("/supervisors", sc.ConvertSupervisorToEmployee)
 	v1.GET("/supervisors", sc.GetSupervisors)
-	v1.GET("/supervisors/:id", sc.GetSupervisorByID)
+	v1.GET("/supervisors/:id", sc.GetSupervisorById)
 	v1.PUT("/supervisors/:id", sc.UpdateSupervisor)
-	v1.DELETE("/supervisors/:id", sc.DeleteEmployee)
+	v1.DELETE("/supervisors/:id", sc.DeleteSupervisor)
 
 	return router
 }
