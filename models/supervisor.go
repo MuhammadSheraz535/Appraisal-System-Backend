7d16bf8ad67e75a6b9604f43639dc99aca00c158
MAsadjaved <masadjaved653@gmail.com>
2023-03-01 23:03:54 +0500

    Separate Supervisor Struct

--- models/supervisor.go
+++ models/supervisor.go
@@ -1,11 +1,5 @@
 package models
 
-type Employee struct {
-	ID    uint   `json:"id" gorm:"primaryKey"`
-	Name  string `json:"name"`
-	Email string `json:"email"`
-	Role  string `json:"role"`
-}
 type Supervisor struct {
 	Name  string `json:"name" binding:"required,min=3,max=60"`
 	Email string `json:"email" binding:"required,email"`
