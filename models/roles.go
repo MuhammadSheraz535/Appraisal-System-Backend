eab322c341baf4e04a3c19dde9dad02f8b615392
Hamza Imtiaz <hamza.imtiaz47022@gmail.com>
2023-03-01 14:33:21 +0500

    making rolename unique

--- models/roles.go
+++ models/roles.go
@@ -11,6 +11,6 @@ const (
 
 type Role struct {
 	ID       uint     `gorm:"primaryKey" json:"role_id"`
-	RoleName RoleName `gorm:"size:100; not null" json:"role_name"`
+	RoleName RoleName `gorm:"size:100; not null;unique" json:"role_name"`
 	IsActive bool     `gorm:"not null" json:"is_active"`
 }
