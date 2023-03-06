57d406951fab4f548820bc5f2de8549254c9867f
MAsadjaved <masadjaved653@gmail.com>
2023-03-06 10:04:51 +0500

    Update the role model

--- models/role.go
+++ models/role.go
@@ -2,15 +2,8 @@ package models
 
 type RoleName string
 
-const (
-	ManagementRole RoleName = "Management"
-	SupervisorRole RoleName = "Supervisor"
-	HRRole         RoleName = "HR"
-	EmployeeRole   RoleName = "Employee"
-)
-
 type Role struct {
-	ID       uint     `gorm:"primaryKey" json:"role_id"`
-	RoleName RoleName `gorm:"size:100; not null;unique" json:"role_name"`
-	IsActive bool     `gorm:"not null" json:"is_active"`
+	ID       uint   `gorm:"primaryKey" json:"role_id"`
+	RoleName string `gorm:"size:100; not null;unique" json:"role_name"`
+	IsActive bool   `gorm:"not null" json:"is_active"`
 }
