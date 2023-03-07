9fa5844147b648bd671fcff99968d66e78fdd408
Muhammad Sheraz <mskhan7507@gmail.com>
2023-03-07 15:08:05 +0500

    router file modified

--- routes/router.go
+++ routes/router.go
@@ -2,18 +2,23 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/mrehanabbasi/appraisal-system-backend/controller"
+	controllers "github.com/mrehanabbasi/appraisal-system-backend/controller"
 )
 
 func NewRouter() *gin.Engine {
 	router := gin.Default()
-	v1 := router.Group("/v1/employees")
-	ec := controller.NewEmployeeController()
-	v1.POST("",ec.CreateEmployee)
-	v1.GET("", ec.GetEmployees)
-	v1.GET("/:id", ec.GetEmployee)
-	v1.PUT("/:id", ec.UpdateEmployee)
-	v1.DELETE("/:id", ec.DeleteEmployee)
+	ec := controllers.NewEmployeeController()
 
+	v1 := router.Group("/v1")
+	{
+		employee := v1.Group("/employees")
+		{
+			employee.POST("", ec.CreateEmployee)
+			employee.GET("", ec.GetEmployees)
+			employee.GET("/:id", ec.GetEmployee)
+			employee.PUT("/:id", ec.UpdateEmployee)
+			employee.DELETE("/:id", ec.DeleteEmployee)
+		}
+	}
 	return router
 }
