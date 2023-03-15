973133770008ff76b32a124c91634d3a82db85c5
Muhammad Sheraz <mskhan7507@gmail.com>
2023-03-15 19:29:21 +0500

    func added for db query for supervisor

---
+++ database/supervisor.go
@@ -0,0 +1,100 @@
+package database
+
+import (
+	"errors"
+
+	"github.com/mrehanabbasi/appraisal-system-backend/models"
+	"gorm.io/gorm"
+)
+
+type SupervisorService struct {
+	db *gorm.DB
+}
+
+func NewSupervisorService() *SupervisorService {
+	db := DB
+	db.AutoMigrate(&models.Employee{})
+	return &SupervisorService{db: db}
+}
+
+const supervisorRoleName = "supervisor"
+
+// Create Supervisor (Database)
+func CreateSupervisor(db *gorm.DB, name string, email string, roleName string, roleID uint) (*models.Employee, error) {
+	// Check if the email already exists in the employees table
+	var existingEmployee models.Employee
+	if err := db.Table("employees").Where("email = ?", email).First(&existingEmployee).Error; err == nil {
+		return nil, errors.New("email already exists")
+	}
+
+	// Create a new Employee object
+	employee := models.Employee{
+		Name:   name,
+		Email:  email,
+		Role:   roleName,
+		RoleID: roleID,
+	}
+
+	// Save the employee to the employees table
+	if err := db.Table("employees").Create(&employee).Error; err != nil {
+		return nil, err
+	}
+
+	return &employee, nil
+}
+
+// GET Supervisors from employee table(with query parameter) Database
+func (sc *SupervisorService) GetSupervisorsWithQuery(name string) ([]models.Employee, error) {
+	var supervisors []models.Employee
+	if name != "" {
+		// Get supervisors that match the specified name
+		if err := sc.db.Table("employees").Where("role = ? AND name LIKE ?", supervisorRoleName, "%"+name+"%").Find(&supervisors).Error; err != nil {
+			return nil, err
+		}
+	} else {
+		// Get all supervisors
+		if err := sc.db.Table("employees").Where("role = ?", supervisorRoleName).Find(&supervisors).Error; err != nil {
+			return nil, err
+		}
+	}
+	return supervisors, nil
+}
+
+// Get Supervisor by ID (Database)
+func (sc *SupervisorService) GetSupervisorByIdDB(id string) (*models.Employee, error) {
+	// Query the employees table for an employee with the specified ID and role "supervisor"
+	var supervisor models.Employee
+	if err := sc.db.Table("employees").Where("id = ? AND role = ?", id, supervisorRoleName).First(&supervisor).Error; err != nil {
+		return nil, err
+	}
+
+	return &supervisor, nil
+}
+
+// Update Supervisor by ID (Database)
+func (sc *SupervisorService) UpdateSupervisorInDatabase(supervisorId string, req models.Supervisor) error {
+	// Query the employees table for an employee with the specified ID and role "supervisor"
+	var supervisor models.Employee
+	if err := sc.db.Table("employees").Where("id = ? AND role = ?", supervisorId, supervisorRoleName).First(&supervisor).Error; err != nil {
+		return err
+	}
+
+	// Update the supervisor's name and email in the employees table
+	supervisor.Name = req.Name
+	supervisor.Email = req.Email
+	if err := sc.db.Table("employees").Save(&supervisor).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Delete the supervisor from employee table(Database)
+func (sc *SupervisorService) DeleteSupervisorFromDB(supervisorId string) error {
+	// Delete the supervisor with the specified ID from the employees table
+	if err := sc.db.Table("employees").Where("id = ? AND role = ?", supervisorId, supervisorRoleName).Delete(&models.Employee{}).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
