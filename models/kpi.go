27267abf138ac6603b8e2d1c6ed383aa30ce9524
Muhammad_Sheraz <mskhan7507@gmail.com>
2023-04-14 14:57:27 +0500

    Common struct added

--- models/kpi.go
+++ models/kpi.go
@@ -5,19 +5,19 @@ import (
 )
 
 type KpiType struct {
-	ID           uint64 `gorm:"primaryKey" json:"id"`
+	Common
 	KpiType      string `gorm:"not null,unique" json:"kpi_type"`
 	BasicKpiType string `gorm:"not null,unique" json:"basic_kpi_type"`
 }
 
 type AssignType struct {
-	ID           uint64 `gorm:"primaryKey" json:"id"`
+	Common
 	AssignTypeId uint64 `gorm:"not null,unique" json:"assign_type_id"`
 	AssignType   string `gorm:"not null,unique" json:"assign_type"`
 }
 
 type Kpi struct {
-	ID            uint           `gorm:"primaryKey" json:"kpi_id"`
+	Common
 	KpiName       string         `gorm:"size:100;not null;unique" json:"kpi_name"`
 	AssignType    uint64         `gorm:"not null" json:"assign_type"`
 	KpiType       string         `gorm:"not null" json:"kpi_type"`
@@ -26,7 +26,7 @@ type Kpi struct {
 }
 
 type MultiKpi struct {
-	ID            uint                    `json:"kpi_id"`
+	Common
 	KpiName       string                  `json:"kpi_name"`
 	AssignType    uint64                  `json:"assign_type"`
 	KpiType       string                  `json:"kpi_type"`
@@ -35,8 +35,8 @@ type MultiKpi struct {
 }
 
 type MultiStatementKpiData struct {
-	KpiID         uint   `gorm:"foreignKey:KpiID" json:"-"`
-	ID            uint64 `gorm:"primaryKey" json:"-"`
+	Common
+	KpiID         uint64 `gorm:"foreignKey:ID" json:"-"`
 	Statement     string `gorm:"not null" json:"statement"`
 	CorrectAnswer string `gorm:"not null" json:"correct_answer"`
 	Weightage     uint64 `gorm:"not null" json:"weightage"`
