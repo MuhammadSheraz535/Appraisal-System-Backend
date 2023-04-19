b27b6428c3bfa48dd597639a0fca1ce3021e0a76
M. Rehan Abbasi <mrehanabbasi@yahoo.com>
2023-04-19 16:10:47 +0500

    make cors optional via env var

--- routes/router.go
+++ routes/router.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	"os"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 
 	"github.com/gin-contrib/cors"
@@ -12,21 +15,24 @@ func NewRouter() *gin.Engine {
 
 	_ = router.SetTrustedProxies(nil)
 
-	router.Use(cors.New(cors.Config{
-		AllowAllOrigins: true,
-		AllowMethods:    []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders: []string{
-			"Content-Type",
-			"Content-Length",
-			"Accept-Encoding",
-			"Authorization",
-			"Accept",
-			"Origin",
-			"Cache-Control",
-		},
-		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin"},
-		AllowCredentials: false,
-	}))
+	isCorsEnabled, _ := strconv.ParseBool(os.Getenv("ENABLE_CORS"))
+	if isCorsEnabled {
+		router.Use(cors.New(cors.Config{
+			AllowAllOrigins: true,
+			AllowMethods:    []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+			AllowHeaders: []string{
+				"Content-Type",
+				"Content-Length",
+				"Accept-Encoding",
+				"Authorization",
+				"Accept",
+				"Origin",
+				"Cache-Control",
+			},
+			ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin"},
+			AllowCredentials: false,
+		}))
+	}
 
 	ec := service.NewEmployeeService()
 	roleController := service.NewRoleService()
