44a8ffefd07069dde568725041266755e94ad810
Muhammad_Sheraz <mskhan7507@gmail.com>
2023-04-27 21:04:37 +0500

    model file modified for apprasial and apprasial flow

--- models/apprasial.go
+++ models/apprasial.go
@@ -2,20 +2,26 @@ package models
 
 type Apprasial struct {
 	CommonModel
-	ApprasialName   string `json:"apprasial_name" binding:"required" gorm:"not null"`
-	TeamId          uint64 `json:"team_id" binding:"required"`
-	AppraisalFlowID uint64 `json:"appraisal_flow_id" binding:"required" gorm:"not null"`
-	AppraisalFlow   AppraisalFlow
-	SupervisorID    uint64 `json:"supervisor_id" binding:"required"`
-	AppraisalType   string `json:"appraisal_type" binding:"required" gorm:"not null"`
-	AppraisalKpis   []AppraisalKpis
+	ApprasialName    string          `json:"apprasial_name" binding:"required" gorm:"not null"`
+	TeamId           uint64          `json:"team_id" binding:"required"`
+	AppraisalFlowID  uint64          `json:"appraisal_flow_id" binding:"required" gorm:"not null"`
+	AppraisalFlow    AppraisalFlow   `json:"-"`
+	SupervisorID     uint64          `json:"supervisor_id" binding:"required"`
+	AppraisalTypeStr string          `gorm:"not null" json:"apprasial_type"`
+	AppraisalType    AppraisalType   `gorm:"references:AppraisalType;foreignKey:AppraisalTypeStr" json:"-"`
+	AppraisalKpis    []AppraisalKpis `json:"appraisal_kpis" gorm:"foreignKey:ApprasialID;not null"`
 }
 
 type AppraisalKpis struct {
 	CommonModel
-	EmployeeID  uint64 `json:"employee_id" binding:"required"`
-	KpiID       uint64 `json:"kpi_id" binding:"required"`
-	Kpi         Kpi
-	ApprasialId uint64 `json:"appraisal_id" binding:"required"`
-	Status      string `json:"status" binding:"required"`
+	ApprasialID uint64 `json:"-"`
+	EmployeeID  uint64 `json:"employee_id"`
+	KpiID       uint64 `json:"kpi_id" gorm:"not null"`
+	Kpi         Kpi    `json:"-"`
+	Status      string `json:"status"`
+}
+
+type AppraisalType struct {
+	CommonModel
+	AppraisalType string `gorm:"not null;unique" json:"appraisal_type"`
 }
