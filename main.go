0c939cd981419aa8afad129934355edac9b25d74
Muhammad_Sheraz <mskhan7507@gmail.com>
2023-05-09 12:00:53 +0500

    removing enum validation from main.go file

--- main.go
+++ main.go
@@ -3,23 +3,15 @@ package main
 import (
 	"os"
 
-	"github.com/gin-gonic/gin/binding"
-	"github.com/go-playground/validator/v10"
 	"github.com/joho/godotenv"
 	"github.com/mrehanabbasi/appraisal-system-backend/database"
 	"github.com/mrehanabbasi/appraisal-system-backend/logger"
-	"github.com/mrehanabbasi/appraisal-system-backend/models"
 	"github.com/mrehanabbasi/appraisal-system-backend/routes"
 )
 
 func main() {
 	// Load environment variables from .env file
 	_ = godotenv.Load(".env")
-
-	// Initializing custom validator
-	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
-		_ = v.RegisterValidation("enum", models.ValidateEnum)
-	}
 
 	// Initializing logger
 	logger.TextLogInit()
