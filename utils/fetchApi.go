1de92b0d8c63a5d3afdf18e2c56ff6b2c8187081
Hamza Imtiaz <hamza.imtiaz47022@gmail.com>
2023-05-29 11:11:16 +0500

    adding comments for FetchApi functions

--- utils/fetchApi.go
+++ utils/fetchApi.go
@@ -13,15 +13,15 @@ import (
 )
 
 func GetRolesID(empIds []uint16) ([]uint16, error) {
-	tossBaseUrl := os.Getenv("TOSS_BASE_URL")
-	method := http.MethodGet
+	tossBaseUrl := os.Getenv("TOSS_BASE_URL") // Get the TOSS base URL from environment variable
+	method := http.MethodGet                  // HTTP method for sending the request
 
 	var roleIDs []uint16
 
 	for _, empId := range empIds {
-		url := tossBaseUrl + "/api/Employee/" + strconv.FormatUint(uint64(empId), 10)
+		url := tossBaseUrl + "/api/Employee/" + strconv.FormatUint(uint64(empId), 10) // Construct the URL for fetching employee details based on empId
 
-		resp, err := SendRequest(method, url, nil)
+		resp, err := SendRequest(method, url, nil) // Send the HTTP request to the specified URL
 		if err != nil {
 			log.Error(err.Error())
 			return nil, err
@@ -31,28 +31,28 @@ func GetRolesID(empIds []uint16) ([]uint16, error) {
 		if resp.StatusCode != http.StatusOK {
 			errMsg := fmt.Sprintf("failed to get role id against employee id %d. status code: %d", empId, resp.StatusCode)
 			log.Error(errMsg)
-			return nil, errors.New(errMsg)
+			return nil, errors.New(errMsg) // Return an error if the response status code is not OK
 		}
 
-		responseBody, err := ioutil.ReadAll(resp.Body)
+		responseBody, err := ioutil.ReadAll(resp.Body) // Read the response body
 		if err != nil {
 			log.Error(err.Error())
-			return nil, err
+			return nil, err // Return an error if there's an issue reading the response body
 		}
 
 		var employee struct {
-			RoleID uint16 `json:"empRole"`
+			RoleID uint16 `json:"empRole"` // Struct for unmarshaling the JSON response
 		}
 
 		if err := json.Unmarshal(responseBody, &employee); err != nil {
 			log.Error(err.Error())
-			return nil, err
+			return nil, err // Return an error if there's an issue unmarshaling the JSON response
 		}
 
-		roleIDs = append(roleIDs, employee.RoleID)
+		roleIDs = append(roleIDs, employee.RoleID) // Append the extracted role ID to the slice
 	}
 
-	return roleIDs, nil
+	return roleIDs, nil // Return the list of role IDs
 }
 
 func GetEmployeesId(teamID uint16) ([]uint16, error) {
@@ -66,42 +66,42 @@ func GetEmployeesId(teamID uint16) ([]uint16, error) {
 		} `json:"allocateTo"`
 	}
 
-	tossBaseUrl := os.Getenv("TOSS_BASE_URL")
-	method := http.MethodGet
-	url := tossBaseUrl + "/api/Project/GetAllProjects"
+	tossBaseUrl := os.Getenv("TOSS_BASE_URL")          // Get the TOSS base URL from environment variable
+	method := http.MethodGet                           // HTTP method for sending the request
+	url := tossBaseUrl + "/api/Project/GetAllProjects" // Construct the URL for fetching all projects
 
-	resp, err := SendRequest(method, url, nil)
+	resp, err := SendRequest(method, url, nil) // Send the HTTP request to fetch all projects
 	if err != nil {
 		log.Error(err.Error())
-		return nil, err
+		return nil, err 
 	}
 	defer resp.Body.Close()
 
 	responseBody, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Error(err.Error())
-		return nil, err
+		return nil, err 
 	}
 
-	var projects []ProjectResponse
+	var projects []ProjectResponse // Slice to store the unmarshaled project responses
 
 	if err := json.Unmarshal(responseBody, &projects); err != nil {
 		log.Error(err.Error())
 		return nil, err
 	}
 
-	var employeeIDs []uint16
+	var employeeIDs []uint16 
 
 	for _, project := range projects {
-		if project.ProjectDetails.ProjectID == teamID {
+		if project.ProjectDetails.ProjectID == teamID { // Check if the project ID matches the provided team ID
 			for _, allocateTo := range project.AllocateTo {
 				if allocateTo.EmployeeID != project.ProjectDetails.SupervisorID {
-					employeeIDs = append(employeeIDs, allocateTo.EmployeeID)
+					employeeIDs = append(employeeIDs, allocateTo.EmployeeID) // Append employee ID to the slice if it's not the supervisor
 				}
 			}
-			break
+			break // Break the loop as we found the matching project
 		}
 	}
 
-	return employeeIDs, nil
+	return employeeIDs, nil // Return the list of employee IDs
 }
