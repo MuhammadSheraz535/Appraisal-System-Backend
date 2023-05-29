9379c2d816d5530efb2b8b6ab88b6de44fac93bb
Muhammad_Sheraz <mskhan7507@gmail.com>
2023-05-29 11:30:05 +0500

    changing isactive field from bool to pointer

--- models/appraisalFlow.go
+++ models/appraisalFlow.go
@@ -11,7 +11,7 @@ type AppraisalFlow struct {
 	CommonModel
 	FlowName         string        `json:"flow_name" gorm:"type:varchar(255);not null;unique" validate:"required,min=3,max=30"`
 	CreatedBy        uint16        `json:"created_by" gorm:"not null" validate:"required"`
-	IsActive         bool          `json:"is_active" gorm:"not null" validate:"required"`
+	IsActive         *bool          `json:"is_active" gorm:"not null" validate:"required"`
 	TeamId           uint16        `json:"team_id" gorm:"not null" validate:"required"`
 	AppraisalTypeStr string        `json:"appraisal_type" gorm:"not null" validate:"required"`
 	AppraisalType    AppraisalType `json:"-" gorm:"references:AppraisalType;foreignKey:AppraisalTypeStr"`
