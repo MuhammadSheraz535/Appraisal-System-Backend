25d395444d67b015765790cf2da6fb655d10c3bc
Hamza Imtiaz <hamza.imtiaz47022@gmail.com>
2023-06-02 14:44:06 +0500

    creating EmployeeData struct with foreign key from Appraisal struct

--- models/appraisal.go
+++ models/appraisal.go
@@ -16,6 +16,15 @@ type Appraisal struct {
 	AppraisalForID   uint16         `gorm:"not null" json:"appraisal_for_id" binding:"required"`
 	Status           *bool          `gorm:"not null" json:"status" binding:"required"`
 	AppraisalKpis    []AppraisalKpi `gorm:"foreignKey:AppraisalID;not null" json:"appraisal_kpis" binding:"required"`
+	EmployeesList    []EmployeeData `gorm:"foreignKey:AppraisalID;not null" json:"employee_data" binding:"required"`
+}
+
+type EmployeeData struct {
+	CommonModel
+	AppraisalID     uint16 `gorm:"not null" json:"-"`
+	TossEmpID       uint16 `gorm:"not null" json:"emp_id" binding:"required"`
+	Designation     uint16 `gorm:"not null" json:"designation_id"`
+	AppraisalStatus bool   `gorm:"not null" json:"appraisal_status"`
 }
 
 type AppraisalKpi struct {
