e231c089cbc3747dd5b4806e9b35b4a90dc91d16
Hamza Imtiaz <hamza.imtiaz47022@gmail.com>
2023-06-02 14:51:27 +0500

    preload EmployeeData table to get in appraisal

--- controller/appraisal.go
+++ controller/appraisal.go
@@ -44,7 +44,7 @@ func CreateAppraisal(db *gorm.DB, appraisal *models.Appraisal) (*models.Appraisa
 func GetAppraisalByID(db *gorm.DB, appraisal *models.Appraisal, id uint64) error {
 	log.Info("Getting appraisal by ID")
 
-	err := db.Model(&models.Appraisal{}).Preload("AppraisalFlow").Preload("AppraisalFlow.FlowSteps").Preload("AppraisalKpis").Where("id = ?", id).First(&appraisal).Error
+	err := db.Model(&models.Appraisal{}).Preload("AppraisalKpis").Preload("EmployeesList").Where("id = ?", id).First(&appraisal).Error
 	if err != nil {
 		log.Error(err.Error())
 		return err
@@ -56,7 +56,7 @@ func GetAppraisalByID(db *gorm.DB, appraisal *models.Appraisal, id uint64) error
 func GetAllAppraisals(db *gorm.DB, appraisal *[]models.Appraisal) (err error) {
 	log.Info("Getting all appraisals")
 
-	err = db.Model(models.Appraisal{}).Preload("AppraisalFlow").Preload("AppraisalFlow.FlowSteps").Preload("AppraisalKpis").Order("id ASC").Find(&appraisal).Error
+	err = db.Model(models.Appraisal{}).Preload("AppraisalKpis").Preload("EmployeesList").Order("id ASC").Find(&appraisal).Error
 	if err != nil {
 		return err
 	}
