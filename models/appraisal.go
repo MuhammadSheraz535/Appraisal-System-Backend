daa690827d4f2f4cdfc8a842261a13ff0e9ec5c9
Muhammad_Sheraz <mskhan7507@gmail.com>
2023-06-03 11:34:17 +0500

    Add json tag for Appraisal flow struct

--- models/appraisal.go
+++ models/appraisal.go
@@ -2,14 +2,14 @@ package models
 
 type Appraisal struct {
 	CommonModel
-	AppraisalName    string        `gorm:"not null" json:"appraisal_name" binding:"required,min=3,max=20"`
-	AppraisalYear    uint16        `gorm:"not null" json:"appraisal_year" binding:"required,gte=2023"`
-	AppraisalTypeStr string        `gorm:"not null" json:"appraisal_type" binding:"required"`
-	AppraisalType    AppraisalType `gorm:"references:AppraisalType;foreignKey:AppraisalTypeStr" json:"-"`
-	SupervisorID     uint16        `gorm:"not null" json:"supervisor_id" binding:"required"`
-	SupervisorName   string        `json:"supervisor_name,omitempty"`
-	AppraisalFlowID  uint16        `gorm:"not null" json:"appraisal_flow_id" binding:"required"`
-	AppraisalFlow    AppraisalFlow
+	AppraisalName    string         `gorm:"not null" json:"appraisal_name" binding:"required,min=3,max=20"`
+	AppraisalYear    uint16         `gorm:"not null" json:"appraisal_year" binding:"required,gte=2023"`
+	AppraisalTypeStr string         `gorm:"not null" json:"appraisal_type" binding:"required"`
+	AppraisalType    AppraisalType  `gorm:"references:AppraisalType;foreignKey:AppraisalTypeStr" json:"-"`
+	SupervisorID     uint16         `gorm:"not null" json:"supervisor_id" binding:"required"`
+	SupervisorName   string         `json:"supervisor_name,omitempty"`
+	AppraisalFlowID  uint16         `gorm:"not null" json:"appraisal_flow_id" binding:"required"`
+	AppraisalFlow    AppraisalFlow  `json:"appraisal_flow"`
 	AppraisalFor     uint16         `gorm:"not null" json:"appraisal_for"  binding:"required"`
 	AppraisalForName string         `json:"appraisal_for_name,omitempty"`
 	AssignType       AssignType     `gorm:"references:AssignTypeId;foreignKey:AppraisalFor" json:"-"`
