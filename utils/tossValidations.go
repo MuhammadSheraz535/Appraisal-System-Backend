a28cfa5d191d2584c5c9400a369d04bfc2a45c99
Muhammad_Sheraz <mskhan7507@gmail.com>
2023-06-03 16:01:22 +0500

    remove function for check team id

--- utils/tossValidations.go
+++ utils/tossValidations.go
@@ -12,7 +12,7 @@ import (
 	log "github.com/mrehanabbasi/appraisal-system-backend/logger"
 )
 
-func CheckKpiAgainstTossApis(selectedAssignID uint16, assignType string) (int, string, error) {
+func VerifyIdAgainstTossApis(selectedAssignID uint16, assignType string) (int, string, error) {
 	// Check which SelectedAssignID exists in the API
 	tossBaseUrl := os.Getenv("TOSS_BASE_URL")
 
@@ -193,53 +193,6 @@ func CheckIndividualAgainstToss(CreatedBy uint16) (int, error) {
 		return http.StatusBadRequest, err
 	}
 
-	return 0, nil
-}
-
-func CheckTeamAgainstToss(TeamId uint16) (int, error) {
-
-	tossBaseUrl := os.Getenv("TOSS_BASE_URL")
-
-	method := http.MethodGet
-	url := tossBaseUrl + "/api/Project/GetAllProjects"
-
-	resp, err := SendRequest(method, url, nil)
-	if err != nil {
-		log.Error(err.Error())
-		return http.StatusInternalServerError, err
-	}
-	defer resp.Body.Close()
-
-	responseBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Error(err.Error())
-		return http.StatusInternalServerError, err
-	}
-
-	var projects []struct {
-		ProjectDetails struct {
-			ProjectID uint16 `json:"projectId"`
-		} `json:"projectDetails"`
-	}
-
-	if err := json.Unmarshal(responseBody, &projects); err != nil {
-		log.Error(err.Error())
-		return http.StatusInternalServerError, err
-	}
-
-	found := false
-	for _, project := range projects {
-		if project.ProjectDetails.ProjectID == TeamId {
-			found = true
-			break
-		}
-	}
-
-	if !found {
-		err := errors.New("invalid selected team id")
-		log.Error(err.Error())
-		return http.StatusBadRequest, err
-	}
 	return 0, nil
 }
 
