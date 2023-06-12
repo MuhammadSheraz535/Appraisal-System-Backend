1ebf4dc409fa08751ee35d8e6b846247c5eb1bdb
Hamza Imtiaz <hamza.imtiaz47022@gmail.com>
2023-06-12 14:57:42 +0500

    removing commented code

--- service/appraisal.go
+++ service/appraisal.go
@@ -798,66 +798,6 @@ func (r *AppraisalService) GetAppraisalKpisByEmpID(c *gin.Context) {
 	c.JSON(http.StatusOK, appraisalKpi)
 }
 
-// func (r *AppraisalService) Score(c *gin.Context) {
-// 	log.Info("Initializing Score handler function...")
-
-// 	appraisalID := c.Param("id")
-// 	employeeID := c.Param("emp_id")
-
-// 	var score models.Score
-
-// 	err := c.ShouldBindJSON(&score)
-// 	if err != nil {
-// 		log.Error(err.Error())
-// 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-// 		return
-// 	}
-
-// 	// Retrieve the appraisal KPI data from the database
-// 	var appraisalKpi models.AppraisalKpi
-// 	err = r.Db.Model(&models.AppraisalKpi{}).Where("appraisal_id = ? AND employee_id = ?", appraisalID, employeeID).First(&appraisalKpi).Error
-// 	if err != nil {
-// 		if errors.Is(err, gorm.ErrRecordNotFound) {
-// 			c.JSON(http.StatusNotFound, gin.H{"error": "Record not found"})
-// 		} else {
-// 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-// 		}
-// 		return
-// 	}
-
-// 	// Check KPI type and set values accordingly
-// 	switch appraisalKpi.Kpi.KpiTypeStr {
-// 	case constants.FEEDBACK_KPI_TYPE, constants.OBSERVATORY_KPI_TYPE:
-// 		fmt.Println("KPI Type :", appraisalKpi.Kpi.KpiTypeStr)
-// 		score.Score = 0
-
-// 	case constants.QUESTIONNAIRE_KPI_TYPE:
-// 		fmt.Println("KPI Type: ", appraisalKpi.Kpi.KpiTypeStr)
-// 		if score.Score != 0 && score.Score != 1 {
-// 			errMsg := "Questionnaire Score should be either 0 or 1"
-// 			log.Error(errMsg)
-// 			c.JSON(http.StatusBadRequest, gin.H{"error": errMsg})
-// 			return
-// 		}
-// 		score.TextAnswer = ""
-
-// 	case constants.MEASURED_KPI_TYPE:
-// 		fmt.Println("KPI Type: ", appraisalKpi.Kpi.KpiTypeStr)
-// 		score.TextAnswer = ""
-// 	}
-
-// 	// Save the score to the database or perform any necessary operations
-
-// 	scores, err := controller.Score(r.Db, &score)
-// 	if err != nil {
-// 		log.Error(err.Error())
-// 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-// 		return
-// 	}
-
-// 	c.JSON(http.StatusCreated, scores)
-// }
-
 func (r *AppraisalService) Score(c *gin.Context) {
 	log.Info("Initializing Score handler function...")
 
