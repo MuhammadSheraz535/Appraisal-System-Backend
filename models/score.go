07ca339f5decdcaef5d7a75bfc0c3f25531759bc
Hamza Imtiaz <hamza.imtiaz47022@gmail.com>
2023-06-12 16:00:08 +0500

    making score a pointer

--- models/score.go
+++ models/score.go
@@ -5,6 +5,6 @@ type Score struct {
 	AppraisalKpiID uint16       `gorm:"not null;default:0" json:"appraisal_kpi_id" validate:"required"`
 	AppraisalKpi   AppraisalKpi `json:"appraisal_kpi"`
 	EvaluatorID    uint16       `gorm:"not null;default:0" json:"evaluator_id"`
-	Score          uint16       `gorm:"default:0" json:"score,omitempty"`
+	Score          *uint16      `gorm:"default:0" json:"score,omitempty"`
 	TextAnswer     string       `gorm:";default:''"  json:"text_answer,omitempty"`
 }
