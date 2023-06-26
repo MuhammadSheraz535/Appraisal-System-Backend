4750c88ab93d93a9da43ed7aafdc669099c41d93
Hamza Imtiaz <hamza.imtiaz47022@gmail.com>
2023-06-26 14:49:45 +0500

    adding log statements in Supervisor Endpoint

--- service/supervService.go
+++ service/supervService.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/mrehanabbasi/appraisal-system-backend/controller"
 	"github.com/mrehanabbasi/appraisal-system-backend/database"
+	log "github.com/mrehanabbasi/appraisal-system-backend/logger"
 	"github.com/mrehanabbasi/appraisal-system-backend/models"
 )
 
@@ -28,9 +29,11 @@ const supervisorRoleName = "supervisor"
 
 // Create Supervisor (handler)
 func (sc *SupervisorService) ConvertSupervisorToEmployee(c *gin.Context) {
+	log.Info("Initializing ConvertSupervisorToEmployee handler function...")
 	// Get the supervisor data from the request body
 	var req models.Supervisor
 	if err := c.ShouldBindJSON(&req); err != nil {
+		log.Error(err.Error())
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
@@ -38,6 +41,7 @@ func (sc *SupervisorService) ConvertSupervisorToEmployee(c *gin.Context) {
 	// Get the supervisor role from the roles table
 	var supervisorRole models.Role
 	if err := sc.db.Table("roles").Where("role_name = ?", supervisorRoleName).First(&supervisorRole).Error; err != nil {
+		log.Error("supervisor role does not exist")
 		c.JSON(http.StatusBadRequest, gin.H{"error": "supervisor role does not exist"})
 		return
 	}
@@ -45,6 +49,7 @@ func (sc *SupervisorService) ConvertSupervisorToEmployee(c *gin.Context) {
 	// Create a new employee with supervisor role
 	employee, err := controller.CreateSupervisor(sc.db, req.Name, req.Email, supervisorRoleName, uint(supervisorRole.ID))
 	if err != nil {
+		log.Error(err.Error())
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
@@ -62,6 +67,7 @@ func (sc *SupervisorService) GetSupervisors(c *gin.Context) {
 
 	supervisors, err := controller.GetSupervisorsWithQuery(sc.db, name)
 	if err != nil {
+		log.Error(err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
@@ -72,12 +78,14 @@ func (sc *SupervisorService) GetSupervisors(c *gin.Context) {
 
 // Get Supervisor by ID (handler)
 func (sc *SupervisorService) GetSupervisorById(c *gin.Context) {
+	log.Info("Initializing GetSupervisorById handler function...")
 	// Get the supervisor ID from the request parameters
 	supervisorId := c.Param("id")
 
 	// Get the supervisor from the database
 	supervisor, err := controller.GetSupervisorByIdDB(sc.db, supervisorId)
 	if err != nil {
+		log.Error("supervisor not found")
 		c.JSON(http.StatusNotFound, gin.H{"error": "supervisor not found"})
 		return
 	}
@@ -88,12 +96,14 @@ func (sc *SupervisorService) GetSupervisorById(c *gin.Context) {
 
 // Update Supervisor by ID (Handler)
 func (sc *SupervisorService) UpdateSupervisor(c *gin.Context) {
+	log.Info("Initializing UpdateSupervisor handler function...")
 	// Get the supervisor ID from the request parameters
 	supervisorId := c.Param("id")
 
 	// Get the updated supervisor data from the request body
 	var req models.Supervisor
 	if err := c.ShouldBindJSON(&req); err != nil {
+		log.Error(err.Error())
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
@@ -101,9 +111,11 @@ func (sc *SupervisorService) UpdateSupervisor(c *gin.Context) {
 	// Update the supervisor in the database
 	if err := controller.UpdateSupervisorInDatabase(sc.db, supervisorId, req); err != nil {
 		if err == gorm.ErrRecordNotFound {
+			log.Error("supervisor not found")
 			c.JSON(http.StatusNotFound, gin.H{"error": "supervisor not found"})
 			return
 		}
+		log.Error(err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
@@ -111,6 +123,7 @@ func (sc *SupervisorService) UpdateSupervisor(c *gin.Context) {
 	// Query the employees table for the updated supervisor
 	var updatedSupervisor models.Employee
 	if err := sc.db.Table("employees").Where("id = ?", supervisorId).First(&updatedSupervisor).Error; err != nil {
+		log.Error(err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
@@ -121,11 +134,13 @@ func (sc *SupervisorService) UpdateSupervisor(c *gin.Context) {
 
 // DeleteSupervisor deletes a supervisor from the employee table (handler)
 func (sc *SupervisorService) DeleteSupervisor(c *gin.Context) {
+	log.Info("Initializing DeleteSupervisor handler function...")
 	// Get the supervisor ID from the request parameters
 	supervisorId := c.Param("id")
 
 	// Call the database function to delete the supervisor
 	if err := controller.DeleteSupervisorFromDB(sc.db, supervisorId); err != nil {
+		log.Error("supervisor not found")
 		c.JSON(http.StatusNotFound, gin.H{"error": "supervisor not found"})
 		return
 	}
